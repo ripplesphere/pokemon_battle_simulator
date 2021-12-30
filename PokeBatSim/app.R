d991d400ead8e8a9d160aad4a2bcbf61f2a57558
George Mills <george.r.mills@gmail.com>
2021-12-30 10:13:06 -0600

    battle routine working

--- PokeBatSim/app.R
+++ PokeBatSim/app.R
@@ -2,23 +2,58 @@ library(shiny)
 library(tidyverse)
 
 data <- read.csv("data/pokemon.csv")
+types <- read.csv("data/type_chart.csv")
 
-# Define UI for application that draws a histogram
+fs <- data %>%
+  filter(Total > 330 & Total < 450) %>%
+  slice( -(183:185)) %>%
+  slice( -(118:119))
+fs <- rbind(fs, data %>% slice(792))
+
+type_1 <- unique(fs$Type.1)
+
+team_comp <- fs %>% slice(sample(1:181, size=6))
+
 ui <- fluidPage(
-  selectizeInput(inputId= "pokeInput", label= "Pokemon", 
-                 choices= sort(unique(data$name)), 
-                 multiple=T,
-                 options = list(plugins= list('remove_button')))
+  fluidRow(
+    column(4,  
+        fluidRow(column(6, 
+
+           selectInput("poke1_type", label = "", 
+                       choices = c("Type", type_1),
+                       selected = 1)
+          ), column(6,
+           selectInput("pokemon1", label = "", 
+                       choices = c("Select", sort(unique(fs$Name))),
+                       selected = 1)
+        )),
+        fluidRow(column(12, verbatimTextOutput("poke1_val")))
+           
+    ),
+    column(4,
+           selectInput("poke2_type", label = "", 
+                       choices = c("Type", type_1),
+                       selected = 1),
+           selectInput("pokemon2", label = h3("Select Pokemon"), 
+                       choices = c("Select", sort(unique(fs$Name))),
+                       selected = 1),
+    ),
+    column(4,
+           selectInput("poke3_type", label = "", 
+                       choices = c("Type", type_1),
+                       selected = 1),
+           selectInput("pokemon3", label = h3("Select Pokemon"), 
+                       choices = c("Select", sort(unique(fs$Name))),
+                       selected = 1),
+    )
+  )
   
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  df_filtered= reactive({
-    data %>%
-      {if (is.null(data$name)) . else filter(., name %in% data$name)}
-  })
-  output$tab= renderTable(df_filtered())
+  
+  output$poke1_val <- renderPrint({ input$pokemon1 })
+  
 }
 
 # Run the application 
--- PokeBatSim/test001.R
+++ PokeBatSim/test001.R
@@ -1,72 +1,72 @@
-library(tidyverse)
-library(ggplot2)
-library(plotly)
+# library(tidyverse)
+# library(ggplot2)
+# library(plotly)
+# 
+# set.seed(339)
 
-set.seed(339)
-
-data <- read.csv("data/pokemon.csv")
-types <- read.csv("data/type_chart.csv")
+# data <- read.csv("data/pokemon.csv")
+# types <- read.csv("data/type_chart.csv")
 
 # This let's us find the values for Pokemon in the second quartile
-plot_ly(y = data$Total, type = "box")
+# plot_ly(y = data$Total, type = "box")
 
 # Get the set of Pokemon in the second quartile minus variations on Pokemon
-fs <- data %>% 
-  filter(Total > 330 & Total < 450) %>%
-  slice( -(183:185)) %>%
-  slice( -(118:119)) 
 #  Add a flying type 1 Pokemon as there was none in q2
-fs <- rbind(fs, data %>% slice(792))
-
 # Get six random Pokemon for each team
-team_user <- fs %>% slice(sample(1:181, size=6))
-team_comp <- fs %>% slice(sample(1:181, size=6))
+
+# fs <- data %>% 
+#   filter(Total > 330 & Total < 450) %>%
+#   slice( -(183:185)) %>%
+#   slice( -(118:119)) 
+# fs <- rbind(fs, data %>% slice(792))
+# 
+# team_user <- fs %>% slice(sample(1:181, size=6))
+# team_comp <- fs %>% slice(sample(1:181, size=6))
+
 
 # Determine the faster of the team's two first Pokemon and start the battle
-ifelse (team_user[1,"Speed"] > team_comp[1,"Speed"],
-        start_battle(c("user", "comp"),list(team_user, team_comp)), 
-        start_battle(c("comp", "user"),list(team_comp, team_user)))
-
-# This function will determine the winning team
-start_battle <- function(team_names,team_teams) {
-  winner <- calc_def(1,1)
+speed_test <- function() {
   
-  # This function acts as the battle mechanism for each team
-  #  calling itself switching which team it is representing each time
-  calc_def <- function(team1_poke,team2_poke) {
-    # Determine the attack modifier for the attacking Pokemon against the
-    #  defending Pokemon
-    attack_modifier <- types %>% 
-      filter(Attacking == team_teams %>% pluck(1, 1, "Type.1") %>% 
-               `[[`(team1_poke)) %>% 
-      pull(team_teams %>% pluck(2, 1, "Type.1") %>% `[[`(team2_poke))
-    # Calculate the defense remaining for the Pokemon after the attack
-    remaining_def <- team_teams %>% pluck(2, 1, "Defense")  %>% 
-      `[[`(team2_poke) -
-      team_teams %>% pluck(1, 1, "Attack")  %>% 
-      `[[`(team1_poke) * attack_modifier
-    # here, or somewhere, will need to determine counter attack, switch
-    #  to next Pokemon, or team defeated
-    
-    team_names <- replace(team_names, c(1,2), team_names[c(2,1)])
-    team_teams <- list(team_teams[2], team_teams[1])
-    calc_def(team2_poke, team1_poke)
+  if (team_user[1,"Speed"] > team_comp[1,"Speed"]) {
+    battle("user", 1, team_user, "comp", 1, team_comp)
+  } else {
+    battle("comp", 1, team_comp, "user", 1, team_user)
   }
   
 }
 
-                     
+battle <- function(atk_name,atk_poke,atk_team,def_name,def_poke,def_team) {
+  attack_modifier <- types %>%
+    filter(Attacking == atk_team$Type.1[atk_poke]) %>%
+    pull(def_team$Type.1[def_poke])
+  
+  remaining_def <- def_team$Defense[def_poke] -
+    atk_team$Attack[atk_poke] * attack_modifier
+  
+  if (remaining_def <= 0) {
+    cat(atk_team$Name[atk_poke],
+        " beats ", def_team$Name[def_poke], "\n")
+    def_poke <- def_poke + 1
+    if (def_poke > 6) {
+      return(atk_name)
+    }
+  } else {
+    def_team$Defense[def_poke] <- remaining_def
+  }
+
+  battle(def_name,def_poke,def_team,atk_name,atk_poke,atk_team)
+  
+}
+
+speed_test()
 
 
 
+# tmp1 <- fs %>% slice(sample(1:181, size=6))
+# tmp2 <- fs %>% slice(sample(1:181, size=6))
+# 
 
-
-
-
-
-
-
-
+unique(fs$Type.1)
 
 
 
